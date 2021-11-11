ebef843680d5e741ccf7fa3e0f8ccd4f54fa28d6
Marcos Mamede <marcospaulo.20@gmail.com>
2021-11-10 23:19:22 -0400

    Fix: -- Ajuste na tabela

--- resources/views/admin/security/users/list.blade.php
+++ resources/views/admin/security/users/list.blade.php
@@ -1,8 +1,8 @@
 <div class="card card-info card-outline">
     <div class="card-footer">
-        {{-- {{ $users->appends($_GET)->links('pagination::adminlte-paginate') }} --}}
+        {{ $users->appends($_GET)->links('pagination::adminlte-paginate') }}
     </div>
-    <div class="card-body pt-1 pb-0">
+    <div class="card-body pt-1 pb-0 pl-2 pr-2">
         <div class="row">
             @if ($users->isEmpty())
                 <div class="alert alert-info col-12">
