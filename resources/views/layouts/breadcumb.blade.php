4cfbeea8a5734eafd0ecca6fc32acbc11a3b59c8
Marcos.Mamede <marcospaulo.20@gmail.com>
2021-11-17 16:47:26 -0400

    Feat: -- Esboço breadcumb

--- resources/views/layouts/breadcumb.blade.php
+++ resources/views/layouts/breadcumb.blade.php
@@ -2,20 +2,22 @@
 {{-- {{ var_dump(Request::path()) }} --}}
 {{-- {{ var_dump(Auth::user()->perfis) }} --}}
 {{-- {{ dd(Auth::user()->operacoesAtribuidas()) }} --}}
-<div class="row">
-    <div class="col-12">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="{{ route('home') }}">
-                    Home
-                </a>
-            </li>
-            @foreach (Auth::user()->operacoesAtribuidas() as $operacao)
-                @if (Request::route()->getAction()['as'] == $operacao->url)
-                    <li class="breadcrumb-item">{{ $operacao->submodulo->nome }}</li>
-                    <li class="breadcrumb-item active">{{ $operacao->nome }}</li>
-                @endif
-            @endforeach
-        </ol>
+@if (Request::route()->getAction()['as'] !== 'home')
+    <div class="row">
+        <div class="col-12">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="{{ route('home') }}">
+                        Home
+                    </a>
+                </li>
+                @foreach (Auth::user()->operacoesAtribuidas() as $operacao)
+                    @if (Request::route()->getAction()['as'] == $operacao->url)
+                        <li class="breadcrumb-item">{{ $operacao->submodulo->nome }}</li>
+                        <li class="breadcrumb-item active">{{ $operacao->nome }}</li>
+                    @endif
+                @endforeach
+            </ol>
+        </div>
     </div>
-</div>
+@endif
